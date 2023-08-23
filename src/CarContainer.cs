9d07e4650f10d6557a8dce2b838dac690e27207c
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-08-23 22:24:15 +0200

    6- Création instance CarContainer

--- src/CarContainer.cs
+++ src/CarContainer.cs
@@ -7,9 +7,14 @@ public class CarContainer
         myCarList = new List<Car>();
     }
 
+    public void AddCar(Car car)
+    {
+        myCarList.Add(car);
+    }
+
     public override string ToString()
     {
-        string myCarListString = "";
+        string myCarListString = "La liste de voiture :" + Environment.NewLine;
         foreach(Car car in myCarList)
         {
             myCarListString += car.ToString() + Environment.NewLine;
