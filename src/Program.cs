743053103276dba69570b06de199a3a806f43ce8
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-08-24 19:44:12 +0200

    7- Saisie multiple utilisateur

--- src/Program.cs
+++ src/Program.cs
@@ -2,18 +2,22 @@
 
 string? BrandUser;
 string? ModelUser;
-
-Console.WriteLine("Veuillez saisir la Marque et le Modèle de la voiture.");
-Console.Write("Marque : "); 
-BrandUser = Console.ReadLine();
-Console.Write("Modèle : ");
-ModelUser = Console.ReadLine();
-Console.WriteLine($"La voiture a pour Marque {BrandUser} et pour modèle {ModelUser}");
-
-Car myCar = new Car(BrandUser, ModelUser);
-
-Console.WriteLine(myCar.ToString());
-
 CarContainer carContainer = new CarContainer();
-carContainer.AddCar(myCar);
+
+do 
+{
+    Console.WriteLine("Veuillez saisir la Marque et le Modèle de la voiture.");
+    Console.Write("Marque : "); 
+    BrandUser = Console.ReadLine();
+    if(BrandUser != "q")
+    {
+        Console.Write("Modèle : ");
+        ModelUser = Console.ReadLine();
+        Console.WriteLine($"La voiture a pour Marque {BrandUser} et pour modèle {ModelUser}");
+        Car myCar = new Car(BrandUser, ModelUser);
+        Console.WriteLine(myCar.ToString());
+        carContainer.AddCar(myCar);
+    }
+} while (BrandUser != "q");
+
 Console.WriteLine(carContainer.ToString());
